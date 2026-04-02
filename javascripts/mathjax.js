fd57499f607e76246875b89276b8b3bcc83eae72
Furutachi <kfurufuru@github.com>
2026-04-03 00:15:22 +0900

    Deployed 40f06f5 with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,26 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    macros: {
+      jw:      "{j\\omega}",
+      Ohm:     "\\,\\Omega",
+      ang:     ["\\angle #1", 1],
+      phasor:  ["\\dot{#1}", 1],
+      conj:    ["\\overline{#1}", 1]
+    }
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
